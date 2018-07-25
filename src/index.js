1086f8bbf048e70b38a421f66e857bd556f144aa
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-07-25 12:10:26 -0700

    set up bulma and sass with react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+// REACT
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// CSS
+import 'bulma/css/bulma.min.css';
+import './css/main.css';
+import './css/linearicons.css';
+
+// COMPONENTS
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// ==========
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
