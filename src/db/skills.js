bfaa1a94afc5451b0ab870a91f4bd2135f270a3c
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-07-28 19:02:27 -0700

    added skills as an object

---
+++ src/db/skills.js
@@ -0,0 +1,146 @@
+const skills = [
+  {
+    name: 'Design',
+    icon: '',
+    tools: [
+      {
+        name: 'Adobe Photoshop',
+        icon: ''
+      },
+      {
+        name: 'Adobe Illustrator',
+        icon: ''
+      }
+    ]
+  },
+  {
+    name: 'Development',
+    icon: '',
+    tools: [
+      {
+        name: 'HTML',
+        icon: ''
+      },
+      {
+        name: 'CSS',
+        icon: ''
+      },
+      {
+        name: 'Sass',
+        icon: ''
+      },
+      {
+        name: 'JavaScript',
+        icon: ''
+      },
+      {
+        name: 'JSON',
+        icon: ''
+      },
+      {
+        name: 'jQuery',
+        icon: ''
+      },
+      {
+        name: 'React',
+        icon: ''
+      },
+      {
+        name: 'React Router',
+        icon: ''
+      },
+      {
+        name: 'Redux',
+        icon: ''
+      },
+      {
+        name: 'Gulp',
+        icon: ''
+      },
+      {
+        name: 'Bootstrap',
+        icon: ''
+      },
+      {
+        name: 'Foundation',
+        icon: ''
+      },
+      {
+        name: 'Bulma',
+        icon: ''
+      },
+      {
+        name: 'Wordpress',
+        icon: ''
+      },
+      {
+        name: 'PHP',
+        icon: ''
+      },
+      {
+        name: 'Ajax',
+        icon: ''
+      },
+      {
+        name: 'Axios',
+        icon: ''
+      },
+      {
+        name: 'Node.js',
+        icon: ''
+      },
+      {
+        name: 'Express.js',
+        icon: ''
+      },
+      {
+        name: 'Knex.js',
+        icon: ''
+      },
+      {
+        name: 'PostgreSQL',
+        icon: ''
+      },
+      {
+        name: 'MySQL',
+        icon: ''
+      },
+      {
+        name: 'Git',
+        icon: ''
+      }
+    ]
+  },
+  {
+    name: 'Other Tools',
+    icon: '',
+    tools: [
+      {
+        name: 'Git',
+        icon: ''
+      },
+      {
+        name: 'Hubspot',
+        icon: ''
+      },
+      {
+        name: 'MailChimp',
+        icon: ''
+      },
+      {
+        name: 'HootSuite',
+        icon: ''
+      },
+      {
+        name: 'Buffer',
+        icon: ''
+      },
+      {
+        name: 'HotJar',
+        icon: ''
+      }
+    ]
+  }
+];
+
+export default skills;
