99f93742fac3c6087256aff25176e34652fc312a
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-07-30 23:21:52 -0700

    added some works to works object

--- src/db/works.js
+++ src/db/works.js
@@ -1,6 +1,42 @@
 const works = [
   {
+    name: 'D&D Character Builder'
+  },
+  {
+    name: 'Devin & Samantha'
+  },
+  {
+    name: 'Gonzales Music'
+  },
+  {
+    name: 'GuildHub'
+  },
+  {
+    name: 'Hourglass'
+  },
+  {
+    name: 'Meraki Design Studio'
+  },
+  {
+    name: 'Nino & Vanessa'
+  },
+  {
+    name: 'The Chobocobros'
+  },
+  {
+    name: 'Veil'
+  },
+  {
+    name: 'Veil Creations'
+  },
+  {
+    name: 'Vintage Blossoms'
+  },
+  {
     name: 'WrightStream'
+  },
+  {
+    name: 'Your Divine Addictions'
   }
 ];
 
