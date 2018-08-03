29f8f48fb714a34e396d3440d4aef1015f4d407d
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-08-02 22:35:34 -0700

    added a thumbnail key to works object

--- src/db/works.js
+++ src/db/works.js
@@ -1,54 +1,67 @@
 const works = [
   {
     name: 'D&D Character Builder',
+    thumbnail: '',
     compeleted: false
   },
   {
     name: 'Devin & Samantha',
+    thumbnail: '',
     compeleted: true
   },
   {
     name: 'Gonzales Music',
+    thumbnail: '',
     compeleted: false
   },
   {
     name: 'GuildHub',
+    thumbnail: '',
     compeleted: false
   },
   {
     name: 'Hourglass',
+    thumbnail: '',
     compeleted: false
   },
   {
     name: 'Meraki Design Studio',
+    thumbnail: '',
     compeleted: true
   },
   {
     name: 'Nino & Vanessa',
+    thumbnail: '',
     compeleted: true
   },
   {
     name: 'The Chobocobros',
+    thumbnail: '',
     compeleted: true
   },
   {
     name: 'Veil',
+    thumbnail: '',
     compeleted: true
   },
   {
     name: 'Veil Creations',
+    thumbnail: '',
     compeleted: true
   },
   {
     name: 'Vintage Blossoms',
+    thumbnail: '',
     compeleted: true
   },
   {
     name: 'WrightStream',
+    thumbnail: '',
     compeleted: false
   },
   {
     name: 'Your Divine Addictions',
+    thumbnail: '',
     compeleted: true
   }
 ];
