bc208815105f24a692adeb0e2e32bff11ee5936a
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-08-03 21:52:17 -0700

    added a photos key to works object

--- src/db/works.js
+++ src/db/works.js
@@ -2,66 +2,79 @@ const works = [
   {
     name: 'D&D Character Builder',
     thumbnail: '',
+    photos: [],
     compeleted: false
   },
   {
     name: 'Devin & Samantha',
     thumbnail: '',
+    photos: [],
     compeleted: true
   },
   {
     name: 'Gonzales Music',
     thumbnail: '',
+    photos: [],
     compeleted: false
   },
   {
     name: 'GuildHub',
     thumbnail: '',
+    photos: [],
     compeleted: false
   },
   {
     name: 'Hourglass',
     thumbnail: '',
+    photos: [],
     compeleted: false
   },
   {
     name: 'Meraki Design Studio',
     thumbnail: '',
+    photos: [],
     compeleted: true
   },
   {
     name: 'Nino & Vanessa',
     thumbnail: '',
+    photos: [],
     compeleted: true
   },
   {
     name: 'The Chobocobros',
     thumbnail: '',
+    photos: [],
     compeleted: true
   },
   {
     name: 'Veil',
     thumbnail: '',
+    photos: [],
     compeleted: true
   },
   {
     name: 'Veil Creations',
     thumbnail: '',
+    photos: [],
     compeleted: true
   },
   {
     name: 'Vintage Blossoms',
     thumbnail: '',
+    photos: [],
     compeleted: true
   },
   {
     name: 'WrightStream',
     thumbnail: '',
+    photos: [],
     compeleted: false
   },
   {
     name: 'Your Divine Addictions',
     thumbnail: '',
+    photos: [],
     compeleted: true
   }
 ];
