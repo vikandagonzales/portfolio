7fca312b4242f953c18d8f8636922865e4362d76
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-08-04 20:51:12 -0700

    added a tags key to works object

--- src/db/works.js
+++ src/db/works.js
@@ -3,78 +3,91 @@ const works = [
     name: 'D&D Character Builder',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: false
   },
   {
     name: 'Devin & Samantha',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: true
   },
   {
     name: 'Gonzales Music',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: false
   },
   {
     name: 'GuildHub',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: false
   },
   {
     name: 'Hourglass',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: false
   },
   {
     name: 'Meraki Design Studio',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: true
   },
   {
     name: 'Nino & Vanessa',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: true
   },
   {
     name: 'The Chobocobros',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: true
   },
   {
     name: 'Veil',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: true
   },
   {
     name: 'Veil Creations',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: true
   },
   {
     name: 'Vintage Blossoms',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: true
   },
   {
     name: 'WrightStream',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: false
   },
   {
     name: 'Your Divine Addictions',
     thumbnail: '',
     photos: [],
+    tags: [],
     compeleted: true
   }
 ];
